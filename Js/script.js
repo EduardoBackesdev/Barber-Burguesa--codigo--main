7329f80677c98d6fdb1a9c1f2bf992c13fd8990b
EduardoBackesDev <eduardo.programacao@hotmail.com>
2023-12-17 18:35:29 -0300

    eba

---
+++ Js/script.js
@@ -0,0 +1,23 @@
+// Inicio Script para redirecionamento
+
+function redirecionamento(){ 
+    var appURL = 'https://play.google.com/store/apps/details?id=com.startapp.appbarber&hl=pt_BR&gl=US'
+
+    window.open(appURL, '_blank');
+}
+
+function whats (){
+    var whatsURL = 'https://api.whatsapp.com/send?phone=555492367901&text=Ol%C3%A1,%20gostaria%20de%20agendar%20um%20corte!'
+
+    window.open(whatsURL, '_blank');
+
+}
+
+function redirecionamentoAPP(){
+    
+    var appURLAPP = 'https://apps.apple.com/br/app/appbarber/id1079972583';
+
+    window.open(appURLAPP, '_blank');
+}
+
+// Fim script para redirecionamento
