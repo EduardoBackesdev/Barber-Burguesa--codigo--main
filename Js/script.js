a931189b19a0162bc920202c5d1d255d62593c17
Eduardo Backes <eduardo.programacao@hotmail.com>
2024-01-08 11:43:23 -0300

    commit

--- Js/script.js
+++ Js/script.js
@@ -1,23 +1,10 @@
 // Inicio Script para redirecionamento
 
-function redirecionamento(){ 
-    var appURL = 'https://play.google.com/store/apps/details?id=br.com.starapp.appbarbercli&hl=en_US'
-
-    window.open(appURL, '_blank');
-}
-
 function whats (){
-    var whatsURL = 'https://api.whatsapp.com/send?phone=555492367901&text=Ol%C3%A1,%20gostaria%20de%20agendar%20um%20corte!'
+    var whatsURL = 'https://api.whatsapp.com/send?phone=5554992367901&text=Ol%C3%A1,%20vim%20pelo%20site,%20gostaria%20de%20marcar%20um%20hor%C3%A1rio...'
 
     window.open(whatsURL, '_blank');
 
-}
-
-function redirecionamentoAPP(){
-    
-    var appURLAPP = 'https://apps.apple.com/br/app/appbarber/id1079972583';
-
-    window.open(appURLAPP, '_blank');
 }
 
 // Fim script para redirecionamento
